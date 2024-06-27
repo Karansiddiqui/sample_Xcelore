a478bdc1e6943f7550305abeb2a2afb53ad9e732
Karansiddiqui <karansiddiq@example.com>
2024-06-27 19:12:37 +0530

    update readme

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,7 +2,17 @@ export default function About() {
   return (
     <div className="min-h-screen flex items-center justify-center">
       <div className="max-w-2xl mx-auto p-3 text-center">
-        <div className="text-md dark:text-gray-500 flex flex-col gap-6"></div>
+        <div className="text-md text-xlg flex flex-col gap-6">
+          <p>
+            Designed to enhance user and admin interactions through a
+            streamlined and user-friendly interface. Built with React, Redux,
+            Tailwind CSS, Node.js, Express, and MongoDB, the platform offers
+            seamless user authentication, distinct dashboards for admins and
+            users, and comprehensive admin privileges including user management
+            and profile updates. Experience efficient and engaging user
+            management with our advanced web technologies.
+          </p>
+        </div>
       </div>
     </div>
   );
